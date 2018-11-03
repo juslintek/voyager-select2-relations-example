a71b3f79705f0cf69d6f12ff9d024119cea987a8
Linas Jusys <linas@webmobix.com>
2018-11-04 01:33:16 +0200

    Default seed nescessary tables

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,21 +1,24 @@
 <?php
+
 use Illuminate\Database\Seeder;
-class DatabaseSeeder extends Seeder
-{
+
+class DatabaseSeeder extends Seeder {
 	/**
 	 * Seed the application's database.
 	 *
 	 * @return void
 	 */
-	public function run()
-	{
-		$this->call(VoyagerDatabaseSeeder::class);
-		//$this->call(VoyagerDummyDatabaseSeeder::class);
-	    $this->call(DataRowsTableSeeder::class);
-        $this->call(DataTypesTableSeeder::class);
-        $this->call(MenuItemsTableSeeder::class);
-        $this->call(ProviderPropertiesTableSeeder::class);
-        $this->call(ProviderValuesTableSeeder::class);
-        $this->call(ProvidersTableSeeder::class);
-    }
+	public function run() {
+		$this->call( MenusTableSeeder::class );
+		$this->call( RolesTableSeeder::class );
+		$this->call( PermissionsTableSeeder::class );
+		$this->call( PermissionRoleTableSeeder::class );
+		$this->call( SettingsTableSeeder::class );
+		$this->call( DataRowsTableSeeder::class );
+		$this->call( DataTypesTableSeeder::class );
+		$this->call( MenuItemsTableSeeder::class );
+		$this->call( ProviderPropertiesTableSeeder::class );
+		$this->call( ProviderValuesTableSeeder::class );
+		$this->call( ProvidersTableSeeder::class );
+	}
 }
